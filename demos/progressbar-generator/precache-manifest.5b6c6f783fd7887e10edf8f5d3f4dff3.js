2d5f4fbbfad18cd862303697439199411055a682
throvn <Faceformer1>
2021-01-05 23:05:54 +0100

    Added Progressbar Generator

---
+++ demos/progressbar-generator/precache-manifest.5b6c6f783fd7887e10edf8f5d3f4dff3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49903f66f37f8ac2be46",
+    "url": "/css/about.e55fa9dd.css"
+  },
+  {
+    "revision": "c1c8994201e39220d5c8",
+    "url": "/css/app.4ca861ca.css"
+  },
+  {
+    "revision": "542101e0de90e0b92349",
+    "url": "/css/home.e55fa9dd.css"
+  },
+  {
+    "revision": "6b77de26ce18a3ee6b4d",
+    "url": "/css/vue.0cec6f61.css"
+  },
+  {
+    "revision": "0d965cd2fe7a37758d584e54ee8247f5",
+    "url": "/img/loading.png"
+  },
+  {
+    "revision": "6d849d65149554f6d3bab735d8dd5277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49903f66f37f8ac2be46",
+    "url": "/js/about.2b0b4949.js"
+  },
+  {
+    "revision": "c1c8994201e39220d5c8",
+    "url": "/js/app.9a4771ec.js"
+  },
+  {
+    "revision": "cd4e909c7403795e4353",
+    "url": "/js/chunk-vendors~253ae210.11e4053d.js"
+  },
+  {
+    "revision": "542101e0de90e0b92349",
+    "url": "/js/home.46f075b9.js"
+  },
+  {
+    "revision": "45575a35d0bcef16dc71",
+    "url": "/js/runtime~app.d3312661.js"
+  },
+  {
+    "revision": "6b77de26ce18a3ee6b4d",
+    "url": "/js/vue.caba9bfa.js"
+  },
+  {
+    "revision": "598d54c36bcfb563ebee4f4ecbf6a1db",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
